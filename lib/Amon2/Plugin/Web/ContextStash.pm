84d73065859c9f451763dc85b64c17634d229b66
zentooo <ankerasoy@gmail.com>
2012-02-04 21:05:54 +0900

    implemented autorender feature

--- lib/Amon2/Plugin/Web/ContextStash.pm
+++ lib/Amon2/Plugin/Web/ContextStash.pm
@@ -7,26 +7,66 @@ our $VERSION = '0.01';
 use Class::Method::Modifiers qw/install_modifier/;
 
 
+my $render_called = 0;
 my $store = +{};
+sub _stash { $store }
 
 sub init {
     my ($class, $c, $conf) = @_;
+
+    _enable_stash($c);
+
+    if ( my $root = $conf->{autorender} ) {
+        $conf->{suffix} ||= '.tt';
+        $conf->{index_filename} ||= 'index';
+        _enable_autorender($c, $conf);
+    }
+}
+
+sub _enable_stash {
+    my $c = shift;
     my $webpkg = ref $c || $c;
 
     Amon2::Util::add_method($webpkg, 'stash', \&_stash);
 
     $c->add_trigger("AFTER_DISPATCH" => sub {
         $store = +{};
+        $render_called = 0;
     });
 
     install_modifier($webpkg, "around", "render", sub {
-        my ($orig, $c, $tmplname, $param) = @_;
+        my ($orig, $c, $tmpl_path, $param) = @_;
+        $render_called = 1; # for autorender
         $param ||= $c->stash;
-        $orig->($c, $tmplname, $param);
+        $orig->($c, $tmpl_path, $param);
     });
 }
 
-sub _stash { $store }
+sub _enable_autorender {
+    my ($c, $conf) = @_;
+    my $webpkg = ref $c || $c;
+
+    install_modifier($webpkg, "around", "dispatch", sub {
+        my ($orig, $c, $tmpl_path, $param) = @_;
+        my $res = $orig->($c, $tmpl_path, $param);
+
+        return do {
+            if ( $render_called ) {
+                $res;
+            }
+            else {
+                my $path_info = $c->req->env->{PATH_INFO};
+
+                my $filename = ($path_info =~ /\/$/) ? $conf->{index_filename} : '';
+                $path_info =~ s!^/!!;
+
+                my $tmpl_path = sprintf("%s%s%s", $path_info, $filename, $conf->{suffix});
+
+                $c->render($tmpl_path);
+            }
+        };
+    });
+}
 
 1;
 __END__
